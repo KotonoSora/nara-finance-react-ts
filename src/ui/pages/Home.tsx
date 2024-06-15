efe79951f9c44bd670daba2fd86dae2a60f15124
KotonoSora <26807417+KotonoSora@users.noreply.github.com>
2024-06-15 16:50:28 +0700

    feat(configure): UP-6 configure submodule ui library with alias path

--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+import Hello from "@library/components/Hello";
+import { ButtonTest } from "@library-components/ButtonTest";
+
 const Home: React.FC = () => {
-  return <h2>Home Page</h2>;
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <Hello />
+      <ButtonTest primary label="button from library" />
+    </div>
+  );
 };
 
 export default Home;
